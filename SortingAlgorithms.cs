3d667440d6d5a0ac7d7be39ea1f0c6a7c4c5f891
mpalacz <mpalacz00@gmail.com>
2023-03-03 17:47:48 +0100

    Add project files.

---
+++ SortingAlgorithms.cs
@@ -0,0 +1,61 @@
+﻿namespace Projekt_1
+{
+    internal class SortingAlgorithms
+    {
+        public static void SelectionSort(int[] tab)
+        {
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                int value, min = i;
+
+                for (int j = i + 1; j < tab.Length; j++)
+                    if (tab[j] < tab[min]) min = j;
+
+                value = tab[i];
+                tab[i] = tab[min];
+                tab[min] = value;
+            }
+
+        }
+
+        public static void InsertionSort(int[] tab)
+        {
+            int j, v;
+            for (int i = 1; i < tab.Length; i++)
+            {
+                j = i; v = tab[i];
+                while (tab[j - 1] > v)
+                {
+                    tab[j] = tab[j - 1]; j -= 1;
+                    if (j == 0) break;
+                }
+                tab[j] = v;
+
+            }
+        }
+
+        public static void QuickSort(int[] tab, int low, int high)
+        {
+            int pivotElement = tab[(low + high) / 2];
+            int i = low;
+            int j = high;
+            int value;
+
+            do
+            {
+                while (tab[i] < pivotElement) i++;
+                while (tab[j] > pivotElement) j--;
+                if (i <= j)
+                {
+                    value = tab[i];
+                    tab[i] = tab[j];
+                    tab[j] = value;
+                    j--; i++;
+                }
+            } while (i <= j);
+            if (low < j) QuickSort(tab, low, j);
+            if (high > i) QuickSort(tab, i, high);
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Projekt_1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
